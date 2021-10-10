2e9f99d72943088374f5a3402a62489df6b10620
qdhrb <qdhrb@sina.com>
2021-10-10 17:18:31 +0800

    frame & page

--- dist/snowy-1.0.3.js
+++ dist/snowy-1.0.3.js
@@ -936,9 +936,33 @@ const $S = (function () {
 	class Page extends Ele {
 		constructor(eTag) {
 			super(eTag);
+			this._need_init = true;
 			this.id(nextId('page'));
 		}
+		/**
+		 * 显示
+		 * @override
+		 * @param {*} [params] 参数
+		 */
+		show(params) {
+			super.show(params);
+			if (this._need_init) {
+				delete this._need_init;
+				this.dispatch('snowy_page_init', null, 0);
+			}
+			this.dispatch('snowy_page_show', null, 0);
+		}
+		/**
+		 * 隐藏
+		 * @override
+		 */
+		hide() {
+			super.hide();
+			this.dispatch('snowy_page_hide', null, 0);
+		}
 	}
+	// 注册：默认页面
+	Page.register('page');
 
 	/** Frame */
 	class Frame extends Ele {
